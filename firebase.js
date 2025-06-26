b91a56e890e850f1c020600d189b87ca13eab110
lucifer-eason <3952001713@qq.com>
2025-06-26 19:32:34 +0800

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,11 +1,12 @@
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
-};
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDMZe2UpzMjaXCr2Jbr9OhofcWGO83z_hI",
+  authDomain: "reasync.firebaseapp.com",
+  projectId: "reasync",
+  storageBucket: "reasync.firebasestorage.app",
+  messagingSenderId: "892894738668",
+  appId: "1:892894738668:web:826f882bd0ab8fe0060dc9",
+  measurementId: "G-KGDDN74GZ5"
+};
 firebase.initializeApp(firebaseConfig);
-firebase.firestore();
+window.auth = firebase.auth();
